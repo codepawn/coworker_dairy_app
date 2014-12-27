ba5a87449f70f70126515b8565c2a771ccac34a8
Lafull <codepawn@icloud.com>
2014-12-28 08:54:24 +0900

    Added remove button

--- client/helper_test.js
+++ client/helper_test.js
@@ -9,6 +9,7 @@ Router.route('/', function () {
 
 Router.route('post');
 Router.route('dairys');
+Router.route('approval');
 
 Template.front.helpers({
   dairys: function () {
@@ -18,9 +19,28 @@ Template.front.helpers({
 
 Template.post.events({
   'submit form': function (evt, tmpl) {
-    Dairys.insert({
-      title: tmpl.find('input[name=title]').value,
-      content: tmpl.find('input[name=content]').value
-    });
+    var title = tmpl.find('input[name=title]').value;
+    var content = tmpl.find('textarea[name=content]').value;
+    if (title.length === 0) {
+
+    } else {
+      Dairys.insert({
+        title: title,
+        content: content
+      });
+      Router.go('front');
+    }
+
   }
 });
+
+Template.dairysItem.events({
+    'click button[id=remove]':function(){
+    Dairys.remove({_id:this._id});
+    }
+});
+
+Template.approval.rendered = function(){
+
+
+};
--- server/init.js
+++ server/init.js
@@ -1,10 +1,17 @@
 Meteor.startup(function () {
   if (Dairys.find().count() === 0) {
-    for (var i = 0; i < 6; i++) {
-      Dairys.insert({
-        title: "오늘의 일기" + i,
-        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquid atque blanditiis commodi consectetur cum deserunt doloribus eaque facilis maiores, mollitia nam nisi obcaecati odit, quia temporibus velit vero voluptas."
-      });
+    for (var i = 0; i < 10; i++) {
+      if (i % 2) {
+        Dairys.insert({
+          title: "오늘의 일기" + i,
+          content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquid atque blanditiis commodi consectetur cum deserunt doloribus eaque facilis maiores, mollitia nam nisi obcaecati odit, quia temporibus velit vero voluptas."
+        });
+      } else {
+        Dairys.insert({
+          title: "Lorem ipsum dolor" + i,
+          content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquid atque blanditiis commodi consectetur cum deserunt doloribus eaque facilis maiores, mollitia nam nisi obcaecati odit, quia temporibus velit vero voluptas."
+        });
+      }
     }
     console.log('insert success');
   }
